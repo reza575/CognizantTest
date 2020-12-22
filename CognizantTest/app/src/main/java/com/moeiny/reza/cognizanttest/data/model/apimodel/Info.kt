f38193c2245a76866c8e8c3f9f228fdb7f97a8d1
reza575 <h_moeiny@yahoo.com>
2020-12-22 22:32:47 +1100

    Design Api Models

---
+++ CognizantTest/app/src/main/java/com/moeiny/reza/cognizanttest/data/model/apimodel/Info.kt
@@ -0,0 +1,6 @@
+package com.moeiny.reza.cognizanttest.data.model.apimodel
+
+data class Info(
+    val rows: List<Row>,
+    val title: String
+)
---
+++ CognizantTest/app/src/main/java/com/moeiny/reza/cognizanttest/data/model/apimodel/Row.kt
@@ -0,0 +1,7 @@
+package com.moeiny.reza.cognizanttest.data.model.apimodel
+
+data class Row(
+    val description: String,
+    val imageHref: String,
+    val title: String
+)
